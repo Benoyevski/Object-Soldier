e7dd4aaed6a2d955cbb4a4cc4abd2393635a3426
Benoyevski <ibeno95@mail.ru>
2022-07-13 10:14:52 +0300

    Создал объект с описанием перса

---
+++ soldier.js
@@ -0,0 +1,12 @@
+const soldier = {
+    name: 'Бот',
+    health: 10,
+    weapon: {
+        name: 'ak-47',
+        bullets: 30,
+    },
+    supplies: 3,
+    fire: function() {},
+    reload: function() {},
+    wound: function() {},
+};
