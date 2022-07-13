0ce3136e3f0237cce4bb1e046e80486c63e86f42
Benoyevski <ibeno95@mail.ru>
2022-07-13 10:29:53 +0300

    добавил функционал методам

--- soldier.js
+++ soldier.js
@@ -6,7 +6,25 @@ const soldier = {
         bullets: 30,
     },
     supplies: 3,
-    fire: function() {},
-    reload: function() {},
-    wound: function() {},
+    fire: function() {
+        if (this.weapon.bullets === 0) {
+            return console.log('обойма пуста. Перезарядитесь');
+        }
+        this.weapon.bullets--;
+        console.log('бах-бах');
+    },
+    reload: function() {
+        if (this.supplies === 0) {
+            return console.log('не осталось припасов');
+        }
+        this.weapon.bullets = 30;
+        this.supplies--;
+        console.log('перезарядка...');
+    },
+    wound: function() {
+        this.health--;
+        if (this.health === 0) {
+            console.log('Ты проиграл');
+        }
+    },
 };
