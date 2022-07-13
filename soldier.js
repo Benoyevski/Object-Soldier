c6d5fd3b72292f0f16e6788b00075d892ea4c31d
Benoyevski <ibeno95@mail.ru>
2022-07-13 10:40:08 +0300

    Провел код-ревью и проверил функциональность, все работает как надо

--- soldier.js
+++ soldier.js
@@ -1,30 +1,30 @@
 const soldier = {
-    name: 'Бот',
-    health: 10,
-    weapon: {
-        name: 'ak-47',
-        bullets: 30,
-    },
-    supplies: 3,
-    fire: function() {
-        if (this.weapon.bullets === 0) {
-            return console.log('обойма пуста. Перезарядитесь');
-        }
-        this.weapon.bullets--;
-        console.log('бах-бах');
-    },
-    reload: function() {
-        if (this.supplies === 0) {
-            return console.log('не осталось припасов');
-        }
-        this.weapon.bullets = 30;
-        this.supplies--;
-        console.log('перезарядка...');
-    },
-    wound: function() {
-        this.health--;
-        if (this.health === 0) {
-            console.log('Ты проиграл');
-        }
-    },
+  name: "Бот",
+  health: 10,
+  weapon: {
+    name: "ak-47",
+    bullets: 30,
+  },
+  supplies: 3,
+  fire: function () {
+    if (this.weapon.bullets === 0) {
+      return console.log("обойма пуста. Перезарядитесь");
+    }
+    this.weapon.bullets--;
+    console.log("бах-бах");
+  },
+  reload: function () {
+    if (this.supplies === 0) {
+      return console.log("не осталось припасов");
+    }
+    this.weapon.bullets = 30;
+    this.supplies--;
+    console.log("перезарядка...");
+  },
+  wound: function () {
+    this.health--;
+    if (this.health === 0) {
+      console.log("Ты проиграл");
+    }
+  },
 };
